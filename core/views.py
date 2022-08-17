3801fd82e7bd0d9624966a9e46af87264775d0b8
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-16 19:07:46 -0500

    Extendiendo la App [Core]

--- core/views.py
+++ core/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+    
+</ul>
+"""
+
 
 def home(request):
-    html_response = "<h1>Mi web personal</h1>"
-    for i in range(10):
-        html_response += "<h2>Portada</h2>"
-    return HttpResponse(html_response)
+    return HttpResponse(html_base + "<h2>Portada</h2")
+
+
+def about(request):
+    return HttpResponse(html_base+"<h2>Acerca de</h2><p>Me llamo Maria y soy programadora</p>")
