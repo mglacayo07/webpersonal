96c9e65964970e012a2f2fd11fd6878ed1bf736b
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-17 18:17:54 -0500

    El patrón MVT: Modelo-Vista-Template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
+
 
 # Create your views here.
+def portfolio(request):
+    kw = {'projects': Project.objects.all()}  # Devuelve toda la lista de proyectos
+    return render(request, "portfolio/portfolio.html",kw)
