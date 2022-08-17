62b408e0a30ead9a2e4439cc5cafe50f11f41a46
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-16 19:18:08 -0500

    Práctica: Páginas portafolio y contacto

--- core/views.py
+++ core/views.py
@@ -5,14 +5,35 @@ html_base = """
 <ul>
     <li><a href="/">Portada</a></li>
     <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portfolio/">Portafolio</a></li>
+    <li><a href="/contact/">Contacto</a></li>
     
 </ul>
 """
 
 
 def home(request):
-    return HttpResponse(html_base + "<h2>Portada</h2")
+    return HttpResponse(html_base + """
+        <h2>Portada</h2
+    """)
 
 
 def about(request):
-    return HttpResponse(html_base+"<h2>Acerca de</h2><p>Me llamo Maria y soy programadora</p>")
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Me llamo Maria y soy programadora</p>
+    """)
+
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+        <h2>Portafolio</h2>
+        <p>Algunos de mis trabajos</p>
+    """)
+
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí les comparto mi email para preguntarme dudas: <a href="maito:correo@test.com">correo@test.com</a></p>
+    """)
