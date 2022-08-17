dda5ec7fe63ddbadc53b31b42a58b27e2351e20d
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-16 19:37:15 -0500

    Práctica: Crea los templates about, portfolio y contact

--- core/views.py
+++ core/views.py
@@ -1,36 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-    
-</ul>
-"""
-
 
 def home(request):
     return render(request, "core/home.html")
 
+
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Me llamo Maria y soy programadora</p>
-    """)
+    return render(request, "core/about.html")
 
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-        <h2>Portafolio</h2>
-        <p>Algunos de mis trabajos</p>
-    """)
+    return render(request, "core/portfolio.html")
 
 
 def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aquí les comparto mi email para preguntarme dudas: <a href="maito:correo@test.com">correo@test.com</a></p>
-    """)
+    return render(request, "core/contact.html")
