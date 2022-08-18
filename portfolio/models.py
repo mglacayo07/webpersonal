1a0d319cf57ef72166327ab42d85c0cf1ed0aa95
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-17 19:52:21 -0500

    Práctica: Añadir enlace a los proyectos

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")  # Campo de caracteres
     decription = models.TextField(verbose_name="Descripción")  # Campo de texto
     image = models.ImageField(verbose_name="Imagen", upload_to='projects')  # Campo de imagenes
+    link = models.URLField(null=False,blank=True, verbose_name="Dirección Web")
     created = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de creación")  # Se ejecuna la primera vez que se crea
     updated = models.DateTimeField(auto_now=True,verbose_name="Fecha de actualización")  # Se ejecuata cada que se actualiza una instancia
 
