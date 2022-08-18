93aa5c2071fb90a86ebf1a3cafadf868e37fe029
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-17 19:14:54 -0500

    Anexo. Añadir un logo en el Administrador

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -31,3 +31,9 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+# Custom titles for admin view
+admin.site.site_header = "Proyecto - Web Personal"
+admin.site.index_title = "Base de datos"
+admin.site.site_title = "Django"
